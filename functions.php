088262a782dabf7bec693e5b85171fb26a6566af
Victor Rusu <31989136+ruvictor@users.noreply.github.com>
2021-04-09 21:01:09 -0400

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Storefront automatically loads the core CSS even if using a child theme as it is more efficient
+ * than @importing it in the child theme style.css file.
+ *
+ * Uncomment the line below if you'd like to disable the Storefront Core CSS.
+ *
+ * If you don't plan to dequeue the Storefront Core CSS you can remove the subsequent line and as well
+ * as the sf_child_theme_dequeue_style() function declaration.
+ */
+//add_action( 'wp_enqueue_scripts', 'sf_child_theme_dequeue_style', 999 );
+
+/**
+ * Dequeue the Storefront Parent theme core CSS
+ */
+function sf_child_theme_dequeue_style() {
+    wp_dequeue_style( 'storefront-style' );
+    wp_dequeue_style( 'storefront-woocommerce-style' );
+}
+
+/**
+ * Note: DO NOT! alter or remove the code above this text and only add your custom PHP functions below this text.
+ */
+
+
+/**
+ * Set a minimum order amount for checkout
+ */
+add_action( 'woocommerce_checkout_process', 'wc_minimum_order_amount' );
+add_action( 'woocommerce_before_cart' , 'wc_minimum_order_amount' );
+ 
+function wc_minimum_order_amount() {
+    // Set this variable to specify a minimum order value
+    $minimum = 50;
+
+    if ( WC()->cart->total < $minimum ) {
+
+        if( is_cart() ) {
+
+            wc_print_notice( 
+                sprintf( 'Your current order total is %s — you must have an order with a minimum of %s to place your order ' , 
+                    wc_price( WC()->cart->total ), 
+                    wc_price( $minimum )
+                ), 'error' 
+            );
+
+        } else {
+
+            wc_add_notice( 
+                sprintf( 'Your current order total is %s — you must have an order with a minimum of %s to place your order' , 
+                    wc_price( WC()->cart->total ), 
+                    wc_price( $minimum )
+                ), 'error' 
+            );
+
+        }
+    }
+}
